257cff62def767f071e2c31e1eee895836b02c8d
ELIZA-BETH002 <elizabethnwaugo@gmail.com>
2023-03-25 13:59:54 -0700

    new commit

---
+++ printf/_printf.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <main.h>
+
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int count = 0;
+
+    while (*format) {
+        if (*format == %) {
+            format++;
+            switch (*format) {
+                case c: {
+                    char c = va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case s: {
+                    char *str = va_arg(args, char*);
+                    while (*str) {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+                }
+                case %: {
+                    putchar(%);
+                    count++;
+                    break;
+                }
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
+
